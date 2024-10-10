b0860db45ac6f61f1c98ca0ac9470c9c01603635
Lucas de Oliveira Santos <106331145+Lucasnnn@users.noreply.github.com>
2024-10-10 19:47:49 -0300

    feat: menu count favorites itens

--- src/app/shared/components/shared-components.module.ts
+++ src/app/shared/components/shared-components.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CountFavoritesComponent } from './count-favorites/count-favorites.component';
 import { TitleComponent } from './title/title.component';
 import { ListCharactersComponent } from './list-characters/list-characters.component';
 import { CharacterCardComponent } from './character-card/character-card.component';
@@ -10,13 +9,12 @@ import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    CountFavoritesComponent,
     TitleComponent,
     ListCharactersComponent,
     CharacterCardComponent,
     FavoriteButtonComponent,
   ],
   imports: [CommonModule, MatIconModule, MatButtonModule],
-  exports: [CountFavoritesComponent, TitleComponent, ListCharactersComponent],
+  exports: [TitleComponent, ListCharactersComponent],
 })
 export class SharedComponentsModule {}
